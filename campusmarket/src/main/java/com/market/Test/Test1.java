8c85efa1b87da49511439cfb5abb4acc3132311a
caobo <13629101894@139.com>
2021-06-11 16:43:33 +0800

    异常不能处理

--- campusmarket/src/main/java/com/market/Test/Test1.java
+++ campusmarket/src/main/java/com/market/Test/Test1.java
@@ -1,10 +1,28 @@
 package com.market.Test;
+import com.market.Test.dao.InsertDao;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+import java.io.InputStream;
 
 public class Test1 {
 
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("configuration.xml");
+        System.out.println(inputStream);
+        SqlSessionFactory  factory = builder.build(inputStream);
+
+
+        SqlSession sqlSession = factory.openSession();
+
+        sqlSession.insert("insert");
+
+        InsertDao in  = new InsertDao();
+        in.insertDao();
 
     }
 
