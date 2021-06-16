72b6b7edd57516be01540125c2a1bd8f2ba55bb9
caobo <13629101894@139.com>
2021-06-16 12:42:47 +0800

    接下来整合Spring框架

--- campus-market/src/main/java/controller/run.java
+++ campus-market/src/main/java/controller/run.java
@@ -1,29 +1,8 @@
 package controller;
 
-import dao.MemberDao;
-import dao.UserDao;
-import domain.User;
-
 public class  run {
     public static void main(String[] args) {
 
-//        User user = new User();
-//        user.setName("曹爸爸");
-//        user.setAge(13);
-//        user.setSex(1);
-
-//        调用静态方法
-//       int c = UserDao.insertUser();
-
-//        UserDao.findUserAll();
-//       传统方法
-//        System.out.println(c);
-//        MemberDao dao = new MemberDao();
-//        Member me = new Member();
-//        dao.addUser(user);
-
-        MemberDao memberDao = new MemberDao();
-        memberDao.findUserAll();
 
     }
 }
--- campus-market/src/main/java/dao/UserDao.java
+++ campus-market/src/main/java/dao/UserDao.java
@@ -1,31 +1,11 @@
 package dao;
 
-import domain.User;
-import lib.GetSession;
 import org.apache.ibatis.session.SqlSession;
-
-import java.util.List;
 
 public class UserDao {
 
- private static   SqlSession session = GetSession.getSqlSession();
+   static SqlSession sqlSession = lib.GetSession.getSqlSession();
 
 
-
-
-    public static int insertUser(){
-        System.out.println((session == null )+"USER dao "+"                1");
-      return   session.insert("addUser");
-    }
-
-
-
-
-    public static void  findUserAll(){
-       List<User> userList =   session.selectList("findUserAll");
-       for (User user:userList){
-           System.out.println(user);
-       }
-    }
 
 }
